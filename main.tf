257c619de7065a6aa5258568adbdd8f4878142cc
jenot15 <kaczorek.kamila@gmail.com>
2022-02-04 09:11:05 +0100

    correct output

--- main.tf
+++ main.tf
@@ -30,3 +30,35 @@ resource "aws_iam_role_policy_attachment" "lambda_policy" {
   role       = aws_iam_role.lambda_exec.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+
+data "archive_file" "lambda_files" {
+  type = "zip"
+
+  source_dir  = "${var.lambda_code_path}/${var.function_name}"
+  output_path = "${var.lambda_code_path}/${var.function_name}.zip"
+}
+
+resource "aws_s3_bucket_object" "lambda_files" {
+  bucket = aws_s3_bucket.code_bucket.id
+
+  key    = "${var.lambda_code_path}/${var.function_name}.zip"
+  source = data.archive_file.lambda_files.output_path
+
+  etag = filemd5(data.archive_file.lambda_files.output_path)
+}
+
+
+
+resource "aws_lambda_function" "function" {
+  function_name = "${var.function_name}"
+  s3_bucket = aws_s3_bucket.code_bucket.id
+  s3_key    = var.s3_key
+
+  runtime = var.runtime
+  handler = var.handler
+
+  source_code_hash = data.archive_file.lambda_files.output_base64sha256
+
+  role = aws_iam_role.lambda_exec.arn
+}
