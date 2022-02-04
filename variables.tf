6595f74cbb933aaafa73606b2c9d5bdf8139a0c8
jenot15 <kaczorek.kamila@gmail.com>
2022-02-04 09:06:55 +0100

    add labda code path

--- variables.tf
+++ variables.tf
@@ -4,3 +4,33 @@ variable "code_bucket" {
   description = "s3 bucket for code"
 
 }
+
+variable "function_name" {
+  description = "Function name"
+
+}
+
+variable "lambda_code_path" {
+  description = "lambda_code_path"
+
+}
+
+variable "runtime" {
+  description = "Runtime"
+
+}
+
+variable "handler" {
+  description = "Function handler"
+
+}
+
+variable "s3_key" {
+
+
+}
+
+output "function_arn" {
+    value = aws_lambda_function.function.arn
+}
+
